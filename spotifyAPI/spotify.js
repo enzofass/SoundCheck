bf49520811c134cc511cee6941ea61fa36e8db8c
enzofass <zoefassioli@gmail.com>
2019-05-02 21:32:50 -0400

    added click handler

--- spotifyAPI/spotify.js
+++ spotifyAPI/spotify.js
@@ -1,101 +1,98 @@
 function searchSpotify(token) {
+  $("#run-search").on("click", function(event) {
+    event.preventDefault();
 
-  // Grab text the user typed into the search input, add to the queryParams object
-  queryParams = $("#search-term")
-    .val()
-    .trim();
-  console.log(queryParams);
-  
-  $.ajax({
-    url:
-      "https://api.spotify.com/v1/search?q=" +
-      "santana" +
-      "&" +
-      "type=artist",    
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + token
-    }
-  }).then(function(response, err) {
-    console.log(response);
-    console.log(response.artists.items[0].id);
+    // Grab text the user typed into the search input, add to the queryParams object
+    searchParams = $("#search-term")
+      .val()
+      .trim();
+    console.log(searchParams);
+
     $.ajax({
       url:
-        "https://api.spotify.com/v1/artists/" +
-        response.artists.items[0].id +
-        "/top-tracks?country=us",      
+        "https://api.spotify.com/v1/search?q=" +
+        searchParams +
+        "&" +
+        "type=artist",
       method: "GET",
       headers: {
         Authorization: "Bearer " + token
       }
-    }).then(function(res, err) {
-      console.log(res);
-      let tracklist = [];
-      let tracklistString = "uris=";
-      for (let i=0; i<res.tracks.length; i++) {
-        tracklist.push(res.tracks[i].id);
-        if (i === res.tracks.length -1){
-        tracklistString += `spotify:track:${res.tracks[i].id}`
-        }else {
-          tracklistString += `spotify:track:${res.tracks[i].id},`
-        }
-      }
-      console.log("tracklist var: ", tracklist);
-      console.log(tracklistString);
-      // $("#musicDiv").append(`<iframe src="https://open.spotify.com/embed/track/${res.tracks[0].id}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`);
+    }).then(function(response, err) {
+      console.log(response);
+      console.log(response.artists.items[0].id);
       $.ajax({
         url:
-          "https://api.spotify.com/v1/me",      
+          "https://api.spotify.com/v1/artists/" +
+          response.artists.items[0].id +
+          "/top-tracks?country=us",
         method: "GET",
         headers: {
           Authorization: "Bearer " + token
         }
       }).then(function(res, err) {
-        console.log(res.id);
+        console.log(res);
+        let tracklist = [];
+        let tracklistString = "uris=";
+        for (let i = 0; i < res.tracks.length; i++) {
+          tracklist.push(res.tracks[i].id);
+          if (i === res.tracks.length - 1) {
+            tracklistString += `spotify:track:${res.tracks[i].id}`;
+          } else {
+            tracklistString += `spotify:track:${res.tracks[i].id},`;
+          }
+        }
+        console.log("tracklist var: ", tracklist);
+        console.log(tracklistString);
+        
         $.ajax({
-          url:
-            "https://api.spotify.com/v1/users/" + res.id + "/playlists",      
-          method: "POST",
+          url: "https://api.spotify.com/v1/me",
+          method: "GET",
           headers: {
             Authorization: "Bearer " + token
-          },
-          data: JSON.stringify({
-            name: "Mic Check Playlist",
-            
-          })
-          
+          }
         }).then(function(res, err) {
-          console.log("create playlist:",  res.id);
-          let playlistID = res.id;
+          console.log(res.id);
           $.ajax({
-            url:
-              "https://api.spotify.com/v1/playlists/" + res.id + "/tracks?" + tracklistString,      
+            url: "https://api.spotify.com/v1/users/" + res.id + "/playlists",
             method: "POST",
             headers: {
               Authorization: "Bearer " + token
             },
             data: JSON.stringify({
-              name: "Mic Check Playlist",
-              
+              name: "SoundCheck Playlist"
             })
-            
           }).then(function(res, err) {
-            console.log("populating playlist:",  res);
-            $("#musicDiv").append(`<iframe src="https://open.spotify.com/embed/playlist/${playlistID}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`);
-            // <iframe src="https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-            
-    
+            console.log("create playlist:", res.id);
+            let playlistID = res.id;
+            $.ajax({
+              url:
+                "https://api.spotify.com/v1/playlists/" +
+                res.id +
+                "/tracks?" +
+                tracklistString,
+              method: "POST",
+              headers: {
+                Authorization: "Bearer " + token
+              },
+              data: JSON.stringify({
+                name: "Mic Check Playlist"
+              })
+            }).then(function(res, err) {
+              console.log("populating playlist:", res);
+              $("#musicDiv").append(
+                `<iframe src="https://open.spotify.com/embed/playlist/${playlistID}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
+              );
+              
+            });
           });
-          
-  
         });
-
       });
-      
     });
   });
 }
 
+///////////////////////////////////////// Bearer Token ///////////////////////////////////////////////////////////////////////////
 (function() {
   var stateKey = "spotify_auth_state";
   /**
